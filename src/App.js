98bae3f1933968f85ca48cc81e3e93446f786804
kim4548 <kdst6156@naver.com>
2023-09-11 10:08:21 +0900

    git 설정

--- src/App.js
+++ src/App.js
@@ -1,10 +1,24 @@
+import { useEffect } from "react";
 
 
 
 function App() {
+  const [count ,setCount ]  = useState(0);
+
+  const clickHandle=()=>{
+setCount(count +1)
+  }
+
+  useEffect(()=>{
+console.log("처음 렌더링 ");
+
+  },[])
+
+
   return (
     <div className="App">
- 
+        <p>{count}</p>
+      <button onClick={clickHandle}>Update</button>
     </div>
   );
 }
